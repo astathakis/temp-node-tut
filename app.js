f5cf5ec04b2aab05446b48c680476c91722fb0ce
tasos <tasos_stathakis@yahoo.com>
2024-05-21 17:56:56 +0300

    sec commit

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
-//manual approach
-// npm init(step by step)
-// npm init -y (everything default)
+const { readFile, writeFile } = require('fs').promises
+// const util = require('util')
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
 
-const _ = require('lodash')
+const start = async () => {
+  try {
+    const first = await readFile('./content/first.txt', 'utf8')
+    const second = await readFile('./content/second.txt', 'utf8')
+    await writeFile(
+      './content/result-async2.txt',
+      `THIS IS AWESOME:${first} ${second}!!!`,
+      { flag: 'a' }
+    )
+    console.log(first)
+    console.log(second)
+  } catch (error) {
+    console.log(`oops ${error} !!!!`)
+  }
+}
 
-const items = [1, [2, [3, [4]]]]
+start()
 
-const newItems = _.flattenDeep(items)
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, 'utf8', (err, data) => {
+//       if (err) {
+//         reject(err)
+//       } else {
+//         resolve(data)
+//       }
+//     })
+//   })
+// }
 
-console.log(newItems)
+// getText('./content/first.txt')
+//   .then((result) => console.log(result))
+//   .catch((e) => console.log(e))
